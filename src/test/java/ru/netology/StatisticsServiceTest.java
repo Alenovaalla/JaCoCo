3a09846b38ca3f9989dbe8afd5c3a6cab2d03054
Alla Alenova <alla_alenova@mail.ru>
2020-05-25 11:21:13 +0300

    files chenged

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -12,7 +12,7 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 11};
+        long[] incomesInBillions = {4, 5, 6, 7, 8, 9, 10, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
